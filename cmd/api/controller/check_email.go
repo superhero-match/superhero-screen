7d7c0cab2180fcb64c4e16a15beb35ef0d73b9df
Nikolajus Karpovas <superheromatch@gmail.com>
2020-11-23 14:29:49 +0200

    Update config package

--- cmd/api/controller/check_email.go
+++ cmd/api/controller/check_email.go
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2019 - 2020 MWSOFT
+  Copyright (C) 2019 - 2021 MWSOFT
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -44,15 +44,15 @@ func (ctl *Controller) CheckEmail(c *gin.Context) {
 		return
 	}
 
-	//err := ctl.Service.ES.DeleteIndex()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//
-	//err = ctl.Service.ES.CreateIndex()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
+	err := ctl.Service.ES.DeleteIndex()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ctl.Service.ES.CreateIndex()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	resp, err := ctl.Service.ES.CheckEmailExists(email)
 	if err != nil {
